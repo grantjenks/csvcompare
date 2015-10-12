91eec55793e8e5937a530791ebcbcda60c6c44ce
Grant Jenks <contact@grantjenks.com>
2015-10-12 13:42:43 -0700

    Bump version to 0.2.1

--- csvcompare/__init__.py
+++ csvcompare/__init__.py
@@ -3,8 +3,8 @@ from .algorithm import csv_file_diff
 from .display import html
 
 __title__ = 'csvcompare'
-__version__ = '0.1.2'
-__build__ = 0x000102
+__version__ = '0.2.1'
+__build__ = 0x000201
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2015 Grant Jenks'
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('LICENSE') as f:
 
 setup(
     name='csvcompare',
-    version='0.1.2',
+    version='0.2.1',
     description='CSV file diff and comparison',
     long_description=readme,
     author='Grant Jenks',
