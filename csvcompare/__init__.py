150b92284b15cd05a7e8987a5061f500fc97f3bb
Grant Jenks <contact@grantjenks.com>
2015-10-12 12:33:45 -0700

    Add package info

--- csvcompare/__init__.py
+++ csvcompare/__init__.py
@@ -1,2 +1,9 @@
 from .algorithm import csv_file_diff
 from .display import html
+
+__title__ = 'csvcompare'
+__version__ = '0.1.2'
+__build__ = 0x000102
+__author__ = 'Grant Jenks'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2015 Grant Jenks'
